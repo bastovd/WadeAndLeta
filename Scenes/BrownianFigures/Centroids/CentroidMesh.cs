d2b953ad49384730e7d306aba3d74358d1672aca
Denys Bastov <bastovd@users.noreply.github.com>
2019-12-21 16:02:10 -0800

    Add files via upload

---
+++ Scenes/BrownianFigures/Centroids/CentroidMesh.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class CentroidMesh : MonoBehaviour {
+    [HideInInspector] public Mesh ConstructedMesh;
+    private float radius;
+    public float Radius;
+    private int numVertices;
+    public int NumVertices;
+
+    private void Awake() {
+        ConstructedMesh = new Mesh();
+        GetComponent<MeshFilter>().sharedMesh = ConstructedMesh;
+    }
+
+    void PopulateMesh() {
+        var center = new Vector3(0, 0, 0);
+        var angle = Mathf.PI * 2f / NumVertices;
+        List<Vector3> verts = new List<Vector3>();
+        verts.Add(center);
+        for (int i = 0; i < NumVertices; ++i) {
+            var pointOnCircle = MathExtensions.PointOnCircle(i * angle, Radius);
+            verts.Add(new Vector3(pointOnCircle.x, pointOnCircle.y));
+        }
+        ConstructedMesh.SetVertices(verts);
+
+        List<int> tris = new List<int>();
+        for (int i = 1; i <= NumVertices; ++i) {
+            tris.Add(0);
+            tris.Add(i);
+            if (i == NumVertices) {
+                tris.Add(1);
+            } else {
+                tris.Add(i + 1);
+            }
+        }
+        ConstructedMesh.SetTriangles(tris, 0);
+
+        ConstructedMesh.MarkDynamic();
+        ConstructedMesh.RecalculateBounds();
+        ConstructedMesh.RecalculateNormals();
+        ConstructedMesh.RecalculateTangents();
+    }
+
+    private void OnValidate() {
+        if (radius != Radius || numVertices != NumVertices) {
+            radius = Radius;
+            numVertices = NumVertices;
+            PopulateMesh(); 
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(CentroidMesh))]
+public class CentroidMeshEditor : Editor {
+
+    private void OnEnable() {
+        
+    }
+
+    private void OnDestroy() {
+        
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+    }
+}
+#endif
---
+++ Scenes/BrownianFigures/Testing/TestAnimation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Oculus;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TestAnimation : MonoBehaviour
+{
+    public PerformanceTest PTest;
+    public float Duration = 4f;
+    public float Up = 1f;
+    private float up = 0;
+
+    private void Awake() {
+        up = Up;
+    }
+
+    void AnimateGameObjects() {
+        for (int i = 0; i < PTest.NumObjects; ++i) {
+            PTest.objects[i].transform.DOLocalMoveY(PTest.positions[i].y + up, Duration).SetEase(Ease.InOutQuart);
+        }
+    }
+
+    void AnimateMeshes() {
+        for (int i = 0; i < PTest.NumObjects; ++i) {
+            DOTween.To(() => PTest.positions[i], x => PTest.positions[i] = x, PTest.positions[i] + Vector3.up * up, Duration).SetEase(Ease.InOutQuart);
+        }
+    }
+
+    public void Animate() {
+        if (PTest.TestType == PerformanceTest.PerformanceTestType.DrawGameObject) {
+            AnimateGameObjects();
+        } else {
+            AnimateMeshes();
+        }
+    }
+
+    private void Update() {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown) ||
+                 OVRInput.GetDown(OVRInput.Button.SecondaryThumbstickDown)) {
+            up = -1f * Up;
+            Animate();
+        }
+        else if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickUp) ||
+                 OVRInput.GetDown(OVRInput.Button.SecondaryThumbstickUp)) {
+            up = 1f * Up;
+            Animate();
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TestAnimation))]
+public class TestAnimationEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Animate")) {
+            (target as TestAnimation).Animate();
+        }
+    }
+}
+#endif
